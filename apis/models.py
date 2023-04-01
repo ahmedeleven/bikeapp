c2f2cf4c5429310666f64af2d8c27232fd81fd14
Ahmed <ahmed.massod@gmail.com>
2023-04-01 16:44:06 +0300

    Initial commit

---
+++ apis/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Station(models.Model):
+	nimi = models.CharField(max_length=100)
+	namn = models.CharField(max_length=100)
+	name = models.CharField(max_length=100)
+	osoite = models.CharField(max_length=100)
+	adress = models.CharField(max_length=100)
+	kaupunki = models.CharField(max_length=100)
+	stad = models.CharField(max_length=100)
+	kapasiteet = models.IntegerField(default=0)
+	x = models.FloatField()
+	y = models.FloatField()
+
+
+class Trip(models.Model):
+	departure_time = models.DateField()
+	return_time = models.DateField()
+	departure_station_id = models.ForeignKey(Station, null=True, on_delete=models.SET_NULL)
+	return_station_id = models.ForeignKey(Station, null=True, on_delete=models.SET_NULL)
+	covered_distance = models.IntegerField(default=0)
+	duration = models.IntegerField(default=0)

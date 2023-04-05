87ac6928705c9c7e9147dd924da5a36a4fea9ba2
Ahmed <ahmed.massod@gmail.com>
2023-04-05 16:17:34 +0300

    Updating views.py comments in the frontend app

--- frontend/views.py
+++ frontend/views.py
@@ -7,19 +7,18 @@ import requests
 from django.http import HttpResponse, JsonResponse
 
 
+# test the api by calling the trips endpoint
 def test_api(request):
-	response = requests.get('http://127.0.0.1:8000/api/trips/')
+	response = requests.get(SERVER_URL+'api/trips/')
 	if response.status_code == 200:
 		data = response.json()
-		#return JsonResponse(data)
 		return render(request,"frontend/test.html",{'data':data,})
 	else:
 		return render(request,"frontend/test.html",{'data':'error',})
-		#return JsonResponse({'error': 'Failed to retrieve data from API'})
 
 
 
-
+# Get all the stations paginated by calling the API
 def stations_list(request):
 	if request.GET.get('page'):
 		response = requests.get(SERVER_URL+'api/stations/?page='+request.GET.get('page'))
@@ -32,7 +31,7 @@ def stations_list(request):
 		return render(request,"frontend/stations_list.html", {'data': "Error getting data from server",})
 
 
-
+# Get all the trips paginated
 def trips_list(request):
 	if request.GET.get('page'):
 		response = requests.get(SERVER_URL+'api/trips/?page='+request.GET.get('page'))
@@ -46,14 +45,20 @@ def trips_list(request):
 
 
 
+# Get station details
 def station_details(request,id):
+
+	# Get one station
 	get_station = requests.get(SERVER_URL+'api/stations/'+str(id))
 	station = get_station.json()
 
+	# Get the count of the trips from this station
 	get_trips_from = requests.get(SERVER_URL+'api/stations/'+str(id)+'/trips_from/count/')
 	trips_from = get_trips_from.json()
 
+	# Get the count of the trips to this station
 	get_trips_to = requests.get(SERVER_URL+'api/stations/'+str(id)+'/trips_to/count/')
 	trips_to = get_trips_to.json()
 
+	# Pass the data to the templates
 	return render(request,"frontend/station_details.html", {'station':station, 'trips_from':trips_from, 'trips_to':trips_to})

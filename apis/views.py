9796fd082b96c993037d8e6c21175499f41f5d1c
Ahmed <ahmed.massod@gmail.com>
2023-04-05 16:00:40 +0300

    Updating views.py comments in the api app

--- apis/views.py
+++ apis/views.py
@@ -18,35 +18,49 @@ from rest_framework.pagination import PageNumberPagination
 def import_stations(request):
 	# read the stations from CSV file to a pandas dataframe
 	stations_df = pd.read_csv("https://opendata.arcgis.com/datasets/726277c507ef4914b0aec3cbcfcbfafc_0.csv")
+
 	# remove unnecessary columns from the dataframe
 	stations_df = stations_df.drop(["FID","Operaattor"], axis=1)
+
 	# change all columns to lower case to match our created models
 	stations_df.columns = stations_df.columns.str.lower()
 	stations = stations_df.to_dict(orient="records")
+
 	# insert stations row by row to the database
 	for row in stations:
 		Station.objects.create(**row)
+
 	return HttpResponse("Done..")
 
 
+# import trips to trips table
 def import_trips(request):
+	# Get the data from the datasets and merge them in one dataframe
 	trips1 = pd.read_csv("https://dev.hsl.fi/citybikes/od-trips-2021/2021-05.csv")
 	trips2 = pd.read_csv("https://dev.hsl.fi/citybikes/od-trips-2021/2021-06.csv")
 	trips3 = pd.read_csv("https://dev.hsl.fi/citybikes/od-trips-2021/2021-07.csv")
 	trips_df = pd.concat([trips1,trips2,trips3])
+
+	# Remove unused columns and rename the dataframe column to match the column names in the database
 	trips_df = trips_df.drop(["Departure station name","Return station name"], axis=1)
 	trips_df = trips_df.rename(columns={"Departure":"departure_time","Return":"return_time","Departure station id":"departure_station_id", "Return station id":"return_station_id", "Covered distance (m)":"covered_distance", "Duration (sec.)":"duration"})
+
+	# Replace null values with 0
 	trips_df["covered_distance"] = trips_df["covered_distance"].fillna(0)
 	trips_df["duration"] = trips_df["duration"].fillna(0)
+
+	# Exclude trips that lasts less than 10 seconds and trips with less than 10 meters distance
 	trips_df = trips_df.drop(trips_df[trips_df["duration"] <= 10].index)
 	trips_df = trips_df.drop(trips_df[trips_df["covered_distance"] <= 10].index)
 
+	# Get a list of trip objects
 	trips = trips_df.to_dict(orient="records")
 	trip_objects = [Trip(**row) for row in trips]
 
 	batch_size = 10000  # number of objects to create in a batch
-	num_batches = (len(trip_objects) // batch_size) + 1
+	num_batches = (len(trip_objects) // batch_size) + 1   # number of batches
 
+	# Load data in batches and show progress bar
 	with tqdm(total=len(trip_objects)) as progress_bar:
 	    for i in range(num_batches):
 	        batch = trip_objects[i*batch_size:(i+1)*batch_size]
@@ -56,25 +70,27 @@ def import_trips(request):
 	return HttpResponse("Done...")
 
 
-
+# Function to test the api
 @api_view(['GET'])
 def test_function(request):
 	return Response({'message': 'Hello!!'})
 
 
 
-
+# Show all trips
 @api_view(['GET'])
 def trips_list(request):
-	page_number = request.GET.get('page', 1)
-	page_number = int(page_number)
-	page_size = request.GET.get('page_size', 100)
-	paginator = PageNumberPagination()
-	paginator.page_size = page_size
-	trips = Trip.objects.select_related('departure_station', 'return_station').all()
-	result_page = paginator.paginate_queryset(trips, request)
+	page_number = request.GET.get('page', 1) # current page number
+	page_number = int(page_number) # page number as integer to perform calculations
+	page_size = request.GET.get('page_size', 100) # number of items per page
+	paginator = PageNumberPagination() # call the built in paginator function
+	paginator.page_size = page_size # set the page size to the number of items already set
+	trips = Trip.objects.select_related('departure_station', 'return_station').all()  # Get all the trips and its related stations from the database
+	result_page = paginator.paginate_queryset(trips, request) # the data for the current page
 	serializer = TripSerializer(result_page, many=True, context={'request': request})
 	data = serializer.data
+
+	# pages data
 	pages = {
 		'count': paginator.page.paginator.count,
 		'num_pages': paginator.page.paginator.num_pages,
@@ -96,6 +112,7 @@ def trips_list(request):
 
 
 
+# show all trips the same way than stations 
 @api_view(['GET'])
 def stations_list(request):
 	page_number = request.GET.get('page',1)
@@ -127,17 +144,19 @@ def stations_list(request):
 		'pages': pages,
 	})
 
-	#return Response({'data':result_page})
 
 
+
+# get station details for one single station
 @api_view(['GET'])
 def station_details(request, station_id):
-	#station_id = request.GET.get('id')
 	station = Station.objects.get(id=station_id)
 	serializer = StationSerializer(station)
 	return Response(serializer.data)
 
 
+
+# get trip details for one single trip
 @api_view(['GET'])
 def trip_details(request, trip_id):
 	trip = Trip.objects.get(id=trip_id)
@@ -145,16 +164,8 @@ def trip_details(request, trip_id):
 	return Response(serializer.data)
 
 
-'''@api_view(['GET'])
-def station_trips(request, station_id):
-	#station = Station.objects.get(id=station_id)
-	#trips = Trip.objects.filter(departure_station_id=station_id)
-	trips = Trip.objects.filter(Q(departure_station_id=station_id) | Q(return_station_id=station_id))
-	#Q(destination='New York') | Q(destination='San Francisco')
-	serializer = TripSerializer(trips, many=True)
-	return Response(serializer.data)'''
 
-
+# get all the trips starting from a single station
 @api_view(['GET'])
 def station_trips_departure(request, station_id):
 	trips = Trip.objects.filter(departure_station_id=station_id)
@@ -163,30 +174,28 @@ def station_trips_departure(request, station_id):
 	return Response({'count': trips_count, 'trips': serializer.data})
 
 
-@api_view(['GET'])
-def station_trips_departure(request, station_id):
-	trips = Trip.objects.filter(departure_station_id=station_id)
-	trips_count = trips.count()
-	serializer = TripSerializer(trips, many=True)
-	return Response({'count': trips_count, 'trips': serializer.data})
-
-
-@api_view(['GET'])
-def station_trips_departure_count(request, station_id):
-	trips_count = Trip.objects.filter(departure_station_id=station_id).count()
-	return Response({'count': trips_count})
-
-
-@api_view(['GET'])
-def station_trips_return_count(request, station_id):
-	trips_count = Trip.objects.filter(return_station_id=station_id).count()
-	return Response({'count': trips_count})
-
-
+# get all the trips ends at a single station
 @api_view(['GET'])
 def station_trips_return(request, station_id):
 	trips = Trip.objects.filter(return_station_id=station_id)
 	trips_count = trips.count()
 	serializer = TripSerializer(trips, many=True)
 	return Response({'count': trips_count, 'trips': serializer.data})
+
+
+# get the count of the trips starts at one single station
+@api_view(['GET'])
+def station_trips_departure_count(request, station_id):
+	trips_count = Trip.objects.filter(departure_station_id=station_id).count()
+	return Response({'count': trips_count})
+
+
+# get all count of the trips ends at one single station
+@api_view(['GET'])
+def station_trips_return_count(request, station_id):
+	trips_count = Trip.objects.filter(return_station_id=station_id).count()
+	return Response({'count': trips_count})
+
+
+
 
